0f3b63324f196551112ff21b3a1185c8c086d8d1
jprivet-dev <jean.philippe.rivet@gmail.com>
2024-10-20 00:05:34 +0200

    feat(aliases): simplify all > remove make aliases & _aliases.source.sh

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -1,2 +1,10 @@
 # Usage: $ . scripts/aliases.sh
-make aliases && source scripts/_aliases.source.sh
+
+Y="\033[33m" # YELLOW
+R="\033[0m"  # RESET
+
+printf "🎉 Create ${Y}php${R}, ${Y}composer${R} & ${Y}symfony${R} aliases 🎉‍\n"
+
+function php() { make php p="$*"; }
+function composer() { make composer p="$*"; }
+function symfony() { make symfony p="$*"; }
