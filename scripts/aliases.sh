9aafb6e78bf48ca3ac52b32fcd3a7f9a8664844e
jprivet-dev <jean.philippe.rivet@gmail.com>
2024-10-20 23:51:35 +0200

    feat(aliases): add options (delete & help)

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -1,10 +1,74 @@
-# Usage: $ . scripts/aliases.sh
+# Usage:
+#   . scripts/aliases.sh [options]
 
 Y="\033[33m" # YELLOW
+G="\033[32m" # GREEN
 R="\033[0m"  # RESET
 
-printf "🎉 Create ${Y}php${R}, ${Y}composer${R} & ${Y}symfony${R} aliases 🎉‍\n"
+function aliases_create() {
+  local name="${1}"
+  printf "✅ Create ${G}${name}${R} alias for the ${G}make ${name}${R} command\n"
+  alias ${name}="aliases_make ${name}"
+}
 
-function php() { make php p="$*"; }
-function composer() { make composer p="$*"; }
-function symfony() { make symfony p="$*"; }
+function aliases_delete() {
+  local name="${1}"
+  printf "❌ Delete ${G}${name}${R} alias for the ${G}make ${name}${R} command\n"
+  unalias ${name}
+}
+
+function aliases_make() {
+  local name="${1}"
+  shift
+  make ${name} p="${*}"
+}
+
+function aliases_help() {
+  printf "\n"
+  printf "${Y}Description:${R}\n"
+  printf "  Create or delete aliases for make commands\n"
+  printf "\n"
+  printf "${Y}Usage:${R}\n"
+  printf "  . aliases [options]\n"
+  printf "\n"
+  printf "${Y}Options:${R}\n"
+  printf "  ${G}--help, -h    ${R}Show help\n"
+  printf "  ${G}--delete, -d  ${R}Delete all aliases\n"
+  printf "\n"
+  printf "${Y}Help:${R}\n"
+  printf "  Create all aliases:\n"
+  printf "\n"
+  printf "    ${G}. aliases${R}\n"
+  printf "\n"
+  printf "  Delete all aliases:\n"
+  printf "\n"
+  printf "    ${G}. aliases --delete${R}\n"
+  printf "\n"
+}
+
+function aliases_delete_all() {
+  aliases_delete php
+  aliases_delete composer
+  aliases_delete symfony
+}
+
+function aliases_create_all() {
+  aliases_create php
+  aliases_create composer
+  aliases_create symfony
+  printf "\n"
+  printf "Delete all aliases with ${G}. aliases --delete${R} command\n"
+  printf "Show help with ${G}. aliases --help${R} command\n"
+}
+
+case ${1} in
+-h | --help)
+  aliases_help
+  ;;
+-d | --delete)
+  aliases_delete_all
+  ;;
+*)
+  aliases_create_all
+  ;;
+esac
